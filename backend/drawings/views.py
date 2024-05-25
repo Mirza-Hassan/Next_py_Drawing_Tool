f05c40e2a02ca2130bf63d4e5a0b5d318ebbe7d7
Mirza-Hassan <hassanbarlas50@gmail.com>
2024-05-25 13:24:17 +0500

    Update views.py

--- backend/drawings/views.py
+++ backend/drawings/views.py
@@ -5,10 +5,12 @@ from .serializers import ShapeSerializer
 class ShapeViewSet(viewsets.ModelViewSet):
     queryset = Shape.objects.all()
     serializer_class = ShapeSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]  # Change to AllowAny
 
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user) if self.request.user.is_authenticated else self.queryset
+        if self.request.user.is_authenticated:
+            return self.queryset.filter(user=self.request.user)
+        return self.queryset
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user if self.request.user.is_authenticated else None)

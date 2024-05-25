992e9729aa180bfa6b9cb794b8b291a54299613b
Mirza-Hassan <hassanbarlas50@gmail.com>
2024-05-25 14:00:19 +0500

    Delete backend directory

--- backend/drawings/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Shape
-
-class ShapeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Shape
-        fields = ['id', 'user', 'shape_type', 'geometry', 'created_at']
